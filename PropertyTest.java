0cbf35ec66a0ec3fc5e0d2a5c0fc239d63565ea8
GuilhermePereira202300306 <162429336+GuilhermePereira202300306@users.noreply.github.com>
2024-03-20 09:39:11 +0000

    Nivel 2

--- PropertyTest.java
+++ PropertyTest.java
@@ -10,5 +10,14 @@ public class PropertyTest{
         
         assertEquals(description, property1.getDescription());
         assertEquals(price, property1.getPrice(), 0.01);
+        }
+    
+    @Test
+    public void testToString(){
+        String description = "T3 Monte Belo";
+        double price = 150000.0;
+        
+        Property property1 = new Property(description, price);
+        assertEquals("Descrição: " + description + "\nPreco: " + price + " Euros", property1toString());
     }
 }
